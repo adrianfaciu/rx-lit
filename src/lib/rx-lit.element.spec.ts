5db66e432343b8f955d5369270ed76248295701d
Adrian Fâciu <adrian.faciu@gmail.com>
2020-05-10 18:30:22 +0300

    Improved RxLitElement and tests

--- src/lib/rx-lit.element.spec.ts
+++ src/lib/rx-lit.element.spec.ts
@@ -1,8 +1,15 @@
 import { fixture, expect } from '@open-wc/testing';
+import { from, Observable, Subject } from 'rxjs';
 
 import { RxLitElement } from './rx-lit.element';
 
-class DemoElement extends RxLitElement {}
+class DemoElement extends RxLitElement {
+  streamValues: unknown | undefined;
+
+  setupObservable(stream$: Observable<unknown>) {
+    this.subscribe('streamValues', stream$);
+  }
+}
 window.customElements.define('demo-testing-element', DemoElement);
 
 describe('RxLitElement', () => {
@@ -11,5 +18,73 @@ describe('RxLitElement', () => {
       '<demo-testing-element></demo-testing-element>'
     );
     expect(element).to.be.ok;
+  });
+
+  it('can handle simple Observable', async () => {
+    const element = await fixture<DemoElement>(
+      '<demo-testing-element></demo-testing-element>'
+    );
+    element.setupObservable(from([1]));
+    expect(element.streamValues).equal(1);
+  });
+
+  it('can handle invalid input', async () => {
+    const element = await fixture<DemoElement>(
+      '<demo-testing-element></demo-testing-element>'
+    );
+    expect(() => element.setupObservable(null!)).to.throw(
+      'Invalid Observable!'
+    );
+    expect(() => element.setupObservable({} as Observable<unknown>)).to.throw(
+      'Invalid Observable!'
+    );
+  });
+
+  it('will unsubscribe when destroyed', async () => {
+    const element = await fixture<DemoElement>(
+      '<demo-testing-element></demo-testing-element>'
+    );
+    const stream$ = new Subject();
+    element.setupObservable(stream$);
+
+    stream$.next(1);
+    expect(element.streamValues).equal(1);
+    expect(stream$.observers.length).equal(1);
+
+    element.remove();
+
+    expect(stream$.observers.length).equal(0);
+  });
+
+  it('will unsubscribe when called with a different stream', async () => {
+    const element = await fixture<DemoElement>(
+      '<demo-testing-element></demo-testing-element>'
+    );
+    const stream$ = new Subject();
+    element.setupObservable(stream$);
+
+    stream$.next(1);
+    expect(element.streamValues).equal(1);
+    expect(stream$.observers.length).equal(1);
+
+    element.setupObservable(new Subject());
+
+    expect(stream$.observers.length).equal(0);
+  });
+
+  it('will ignore calls with the same stream', async () => {
+    const element = await fixture<DemoElement>(
+      '<demo-testing-element></demo-testing-element>'
+    );
+    const stream$ = new Subject();
+    element.setupObservable(stream$);
+
+    stream$.next(1);
+    expect(element.streamValues).equal(1);
+    expect(stream$.observers.length).equal(1);
+
+    element.setupObservable(stream$);
+
+    expect(stream$.observers.length).equal(1);
   });
 });
--- src/lib/rx-lit.element.ts
+++ src/lib/rx-lit.element.ts
@@ -1,17 +1,36 @@
 import { LitElement } from 'lit-element';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, Subscription, isObservable } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 const unsubscribe = Symbol('unsubscribe');
+const subscriptions = Symbol('subscriptions');
+
+class ExistingSubscription {
+  stream$?: Observable<unknown>;
+  subscription?: Subscription;
+}
 
 export class RxLitElement extends LitElement {
   [unsubscribe] = new Subject();
+  [subscriptions] = new Map<keyof this, ExistingSubscription>();
 
-  subscribe<Key extends keyof this>(prop: Key, stream: Observable<this[Key]>) {
-    stream.pipe(takeUntil(this[unsubscribe])).subscribe((res) => {
-      this[prop] = res;
-      this.requestUpdate();
-    });
+  subscribe<Key extends keyof this>(prop: Key, stream$: Observable<this[Key]>) {
+    if (!isObservable(stream$)) throw new Error('Invalid Observable!');
+
+    const existingSubscription = this[subscriptions].get(prop);
+    if (existingSubscription) {
+      if (existingSubscription?.stream$ === stream$) return;
+      else existingSubscription?.subscription?.unsubscribe();
+    }
+
+    let subscription = stream$
+      .pipe(takeUntil(this[unsubscribe]))
+      .subscribe((res) => {
+        this[prop] = res;
+        this.requestUpdate();
+      });
+
+    this[subscriptions].set(prop, { stream$, subscription });
   }
 
   disconnectedCallback() {
